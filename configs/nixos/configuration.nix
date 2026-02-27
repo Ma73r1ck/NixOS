dcc36c6b59e0aba15b581a0a6d49f39dd773f775
Ma73r1ck <157492521+Ma73r1ck@users.noreply.github.com>
2026-02-27 13:07:00 +0200

    Update configuration.nix

--- configs/nixos/configuration.nix
+++ configs/nixos/configuration.nix
@@ -135,12 +135,10 @@
   environment.systemPackages = with pkgs; [
     # vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     # wget
-    google-chrome
     vlc
     discord
     kdePackages.partitionmanager
     gnome-disk-utility
-    telegram-desktop
     usbutils
     libfprint-fpcmoh
     vscodium
@@ -149,8 +147,7 @@
     ntfs3g
     qbittorrent-enhanced
     p7zip
-    zoom-us
-    freeoffice
+    urar
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
